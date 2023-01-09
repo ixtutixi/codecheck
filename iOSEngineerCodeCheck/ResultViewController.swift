4f3733879269fef8188480525de10efdf91e378f
ixtutixi <ixtutixi0@icloud.com>
2023-01-10 04:19:05 +0900

    クラス名・変数名の修正
    命名規約に沿った適切なクラス名・変数名への修正

---
+++ iOSEngineerCodeCheck/ResultViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController2.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 史 翔新 on 2020/04/21.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+    @IBOutlet weak var languageLabel: UILabel!
+
+    @IBOutlet weak var starsLabel: UILabel!
+    @IBOutlet weak var watchersLabel: UILabel!
+    @IBOutlet weak var forksLabel: UILabel!
+    @IBOutlet weak var issuesLabel: UILabel!
+
+    var vc1: SearchViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let repository = vc1.repository[vc1.index]
+
+        languageLabel.text = "Written in \(repository["language"] as? String ?? "")"
+        starsLabel.text = "\(repository["stargazers_count"] as? Int ?? 0) stars"
+        watchersLabel.text = "\(repository["wachers_count"] as? Int ?? 0) watchers"
+        forksLabel.text = "\(repository["forks_count"] as? Int ?? 0) forks"
+        issuesLabel.text = "\(repository["open_issues_count"] as? Int ?? 0) open issues"
+        getImage()
+        
+    }
+    
+    func getImage(){
+
+        let repository = vc1.repository[vc1.index]
+
+        titleLabel.text = repository["full_name"] as? String
+
+        if let owner = repository["owner"] as? [String: Any] {
+            if let imageUrl = owner["avatar_url"] as? String {
+                URLSession.shared.dataTask(with: URL(string: imageUrl)!) { (data, respose, error) in
+                    let image = UIImage(data: data!)!
+                    DispatchQueue.main.async {
+                        self.imageView.image = image
+                    }
+                }.resume()
+            }
+        }
+        
+    }
+    
+}
--- iOSEngineerCodeCheck/SearchViewController.swift
+++ iOSEngineerCodeCheck/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  iOSEngineerCodeCheck
 //
 //  Created by 史 翔新 on 2020/04/20.
@@ -8,22 +8,22 @@
 
 import UIKit
 
-class ViewController: UITableViewController, UISearchBarDelegate {
+class SearchViewController: UITableViewController, UISearchBarDelegate {
 
-    @IBOutlet weak var SchBr: UISearchBar!
+    @IBOutlet weak var SearchBar: UISearchBar!
     
-    var repo: [[String: Any]]=[]
+    var repository: [[String: Any]]=[]
     
     var task: URLSessionTask?
     var word: String!
     var url: String!
-    var idx: Int!
+    var index: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        SchBr.text = "GitHubのリポジトリを検索できるよー"
-        SchBr.delegate = self
+        SearchBar.text = "GitHubのリポジトリを検索できるよー"
+        SearchBar.delegate = self
     }
     
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
@@ -42,10 +42,10 @@ class ViewController: UITableViewController, UISearchBarDelegate {
         
         if word.count != 0 {
             url = "https://api.github.com/search/repositories?q=\(word!)"
-            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
-                if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
-                    if let items = obj["items"] as? [[String: Any]] {
-                    self.repo = items
+            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
+                if let object = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
+                    if let items = object["items"] as? [[String: Any]] {
+                    self.repository = items
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
                         }
@@ -61,22 +61,22 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "Detail"{
-            let dtl = segue.destination as! ViewController2
-            dtl.vc1 = self
+            let detail = segue.destination as! ResultViewController
+            detail.vc1 = self
         }
         
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repo.count
+        return repository.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell()
-        let rp = repo[indexPath.row]
-        cell.textLabel?.text = rp["full_name"] as? String ?? ""
-        cell.detailTextLabel?.text = rp["language"] as? String ?? ""
+        let repository = repository[indexPath.row]
+        cell.textLabel?.text = repository["full_name"] as? String ?? ""
+        cell.detailTextLabel?.text = repository["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
         
@@ -84,7 +84,7 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
-        idx = indexPath.row
+        index = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
         
     }
